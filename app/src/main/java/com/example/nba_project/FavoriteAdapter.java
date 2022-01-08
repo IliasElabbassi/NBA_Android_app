5314df818e99607e249114e2faaa43bc2f64cb6b
JeanDeboutGat <jeandebout.gatari.etu@univ-lille.fr>
2022-01-08 22:45:41 +0100

    feat: create favorite adapter

---
+++ app/src/main/java/com/example/nba_project/FavoriteAdapter.java
@@ -0,0 +1,54 @@
+package com.example.nba_project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nba_project.data.entity.FavoriteEntity;
+
+import java.util.List;
+
+
+public class FavoriteAdapter extends  RecyclerView.Adapter<FavoriteAdapter.ViewHolder> {
+    private List<FavoriteEntity> favoriteEntities;
+    Context context;
+
+    public FavoriteAdapter(List<FavoriteEntity> favoriteEntities, Context context) {
+        this.favoriteEntities = favoriteEntities;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.favorite_teams_layout,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        FavoriteEntity fl=favoriteEntities.get(i);
+        viewHolder.fullname.setText(fl.getFullname());
+
+    }
+    @Override
+    public int getItemCount() {
+        return favoriteEntities.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        //ImageView logo;
+        TextView fullname;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+           // logo=(ImageView)itemView.findViewById(R.id.logo);
+            fullname=(TextView)itemView.findViewById(R.id.fullname_team);
+        }
+    }
+
+}
