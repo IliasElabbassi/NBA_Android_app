90f4ee289adce4004fdc4d1760446bd733918265
JeanDeboutGat <jeandebout.gatari.etu@univ-lille.fr>
2022-01-09 13:35:33 +0100

    feat: add app logo

--- app/src/main/java/com/example/nba_project/MainActivity.java
+++ app/src/main/java/com/example/nba_project/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -107,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        getSupportActionBar().setIcon(R.drawable.ic_favorite_shadow_24);
+
 
         MenuItem menuItem = menu.findItem(R.id.search);
         androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) menuItem.getActionView();
